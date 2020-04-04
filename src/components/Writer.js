76763bb52ee59aa221c1e671631a2c4095c4a81e
drkjc <drkjc@protonmail.com>
2020-04-04 16:33:16 -0700

    added simple form to writer

--- src/components/Writer.js
+++ src/components/Writer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/Writer.scss';
 
+
 export default class Writer extends Component {
 
     state = {
@@ -8,9 +9,22 @@ export default class Writer extends Component {
         content: ""
     }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+        console.log(this.state)
+    }
+
     render() {
         return (
-            <div className="writer">Writer</div>
+            <div className="writer">
+                <form>
+                    <label>Title</label><br />
+                    <input type="text" name="title" onChange={this.handleChange} /><br/>
+                    <textarea type="text" name="content" onChange={this.handleChange} />
+                </form>
+            </div>
         )
     }
 }
