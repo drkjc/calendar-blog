1fd33443f02ce1efd6c9367249b02a45797c1d2c
drkjc <drkjc@protonmail.com>
2020-04-04 12:24:32 -0700

    added link path to writer route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Day from "./components/Day";
 import Header from "./components/Header";
 import DayTitles from "./components/DayTitles"
+import Writer from "./components/Writer"
 import "./css/App.scss";
 
 class App extends Component {
@@ -9,20 +11,29 @@ class App extends Component {
   renderCalendar = () => {
     let days = [];
     for(let i = 1; i < 31; i++) {
-      days.push(<Day key={i} />)
+      days.push(
+        <Day key={i} id={i}/>
+      )
     }
+    
     return days;
   }
 
   render() {
     return (
-      <div id="app">
-        <Header />
-        <DayTitles />
-        <div className="grid-container">
-         {this.renderCalendar()}
+      <Router>
+        <div id="app">
+          <Header />
+          <DayTitles />
+          <div className="grid-container">
+            {this.renderCalendar()}
+          </div>
         </div>
-      </div>
+
+        <Switch>
+          <Route exact path="/writer/:id" render={props => <Writer {...props} />}/>
+        </Switch>
+      </Router>
     );
   }
 }
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,7 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Writer from "./Writer"
 import "../css/Day.scss";
 
-export default function Day() {
-  return <div className="day">Day Number</div>;
+export default function Day(props) {
+  return (
+    <Link to={{ pathname: `/writer/${props.id}`}}>
+      <div className="day">{props.id}</div>
+    </Link>
+  )
 }
 
