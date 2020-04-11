05917d9b5e1894be09b5fb4932ac5309c9c4c3d5
drkjc <drkjc@protonmail.com>
2020-04-11 14:06:00 -0700

    added writer reducer

---
+++ src/redux/reducers/writerReducer.js
@@ -0,0 +1,12 @@
+export default function writerReducer(
+    state = {
+        posts: []
+}, action
+) {
+    switch (action.type) {
+        case "CREATE_POST":
+            return {...state};
+        default: 
+            return state;
+    }
+}
