171b1d2b2df1a38bc1a55c51d24183680048563e
drkjc <drkjc@protonmail.com>
2020-04-11 16:11:22 -0700

    posts are re rendered in writer container

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-export default function PostList(props) {
-    debugger;
-    return <div>PostList</div>
+function renderPosts(posts) {
+    if(posts.length) {
+        return posts.map(post => {
+            debugger;
+            return <div key={post.id}>{post.title}</div>
+        })
+    } else {
+        return "create a post"
+    }
+}
+
+export default function PostList({ posts }) {
+
+    return <div>{renderPosts(posts)}</div>
 }
