cf0a19d682f5dd662769a2d42e74e42a0e13c11d
drkjc <drkjc@protonmail.com>
2020-04-18 10:34:14 -0700

    added getDaysArray method to calendar container

--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -5,9 +5,25 @@ import '../css/Calendar.scss'
 
 export default class Calendar extends Component {
 
+  getDaysArray = (year, month) => {
+    const monthIndex = month - 1
+    const names = Object.freeze(
+       [ 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat' ]);
+    const date = new Date(year, monthIndex, 1);
+    const result = [];
+    while (date.getMonth() === monthIndex) {
+      result.push(`${date.getDate()}-${names[date.getDay()]}`);
+      date.setDate(date.getDate() + 1);
+    }
+    return result;
+  }
+
     renderCalendar = () => {
+        let d = new Date();
+        let month = this.getDaysArray(d.getFullYear(), d.getMonth());
+        //debugger;
         let days = [];
-        for(let i = 1; i < 36; i++) {
+        for(let i = 1; i < month.length; i++) {
           days.push(
             <Day key={i} id={i}/>
           )
