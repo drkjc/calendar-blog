84a0b3773df84b80a02385d9e5da4bc375d65a02
drkjc <drkjc@protonmail.com>
2020-04-25 12:41:38 -0700

    added previous and next cases to calendarReducer

--- src/redux/reducers/calendarReducer.js
+++ src/redux/reducers/calendarReducer.js
@@ -9,7 +9,18 @@ export default function calendarReducer(
                 state, { 
                 month: action.payload
             });
-        case "GET_PREVIOUS_MONTH"
+        case "GET_PREVIOUS_MONTH":
+            return Object.assign(
+                {},
+                state, {
+                    month: action.payload
+                });
+        case "GET_NEXT_MONTH":
+            return Object.assign(
+                {},
+                state, {
+                    month: action.payload
+                });
         default: 
             return state;
     }
