4cd72a66d28962442b2f87706dd2af9370eba30b
drkjc <drkjc@protonmail.com>
2020-05-12 17:00:00 -0700

    added home link to slideMenu

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function SlideMenu() {
-    return <div style={{ visibility: "hidden" }}id="slideMenu">Slide Menu</div>
+    return (
+        <div style={{ visibility: "hidden" }}id="slideMenu">
+            <span id="home"><Link to={{ pathname: '/'}}>Home</Link></span>
+            Slide Menu
+        </div>
+    )
 }
